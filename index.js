05c78cd3354c1e4e619e36f49884d1b1be95353d
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-01-02 23:52:14 -0800

    Add weather functionality

---
+++ index.js
@@ -0,0 +1,48 @@
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            // Do something for the current logged-in user here: 
+            console.log(user.uid);
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    var user_Name = userDoc.data().name;
+                    var today = new Date();
+                    var time = today.getHours();
+
+                    if (time >= 6 && time < 12) {
+                        document.getElementById("profile").innerHTML = "Good Morning " + user_Name; //using javascript
+                    } else if (time >= 12 && time < 17) {
+                        document.getElementById("profile").innerHTML = "Good Afternoon " + user_Name;
+                    } else if (time >= 17 && time < 23) {
+                        document.getElementById("profile").innerHTML = "Good Evening " + user_Name;
+
+                    } else {
+                        document.getElementById("profile").innerHTML = "Hi " + user_Name;
+                    }
+                })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
+
+
+// Change background based on time
+
+function setbackgroundImg() {
+    var today = new Date();
+    var time = today.getHours();
+    // console.log(time)
+    if (time > 6 && time < 18) {
+        document.body.style.backgroundImage = "url('images/mazoedam.jpg')";
+    } else if (time >= 18 && time <= 6) {
+        document.body.style.backgroundImage = "url('images/Masvingosunrise.jpg')";
+    }
+}
+
+setbackgroundImg();
