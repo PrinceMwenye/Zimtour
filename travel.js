66ab105e112b3f7aa392b1616984cf874bed60e4
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-01-15 10:17:10 -0800

    Remove redundant material icon

---
+++ travel.js
@@ -0,0 +1,32 @@
+// function writeHikes() {
+//     //define a variable for the collection you want to create in Firestore to populate data
+//     var hikesRef = db.collection("Hikes");
+
+//     hikesRef.add({
+//         code:"BBY01",
+//         name: "Burnaby Lake Park Trail",    //replace with your own city?
+//         city: "Burnaby",
+//         province: "BC",
+//         level: "easy",
+//         length: "10 km",
+//         length_time: "2h 33mm"
+//     });
+//     hikesRef.add({
+//         code:"AM01",
+//         name: "Buntzen Lake Trail Trail",    //replace with your own city?
+//         city: "Anmore",
+//         province: "BC",
+//         level: "moderate",
+//         length: "10.5 km",
+//         length_time: "3h 17mm"
+//     });
+//     hikesRef.add({
+//         code:"NV01",
+//         name: "Mount Seymoure Trail",    //replace with your own city?
+//         city: "North Vancouver",
+//         province: "BC",
+//         level: "hard",
+//         length: "8.2 km",
+//         length_time: "3h 20m"
+//     });
+// }
